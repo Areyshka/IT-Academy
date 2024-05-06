09cca4284c3484d132a399ecb695b3ae6f6e9095
areyshka <elena.draven@mail.ru>
2024-05-06 22:00:12 +0300

    feat: add a function for counting the number of vowels, changing the color of the title h2 depending on the number of vowels, and displaying information about the text

--- Task-6/main.js
+++ Task-6/main.js
@@ -3,6 +3,8 @@ document.getElementById('textInput').addEventListener('input', () => {
     // Получение элемента ввода и элемента заголовка h2
     let input = document.getElementById('textInput');
     let h2 = document.getElementById('result');
+    // Получение количества гласных
+    let count = getVowels(input.value);
     // Проверка на пустое значение ввода
     if (input.value === '') {
         // Скрытие элемента h2, если поле ввода пустое
@@ -11,6 +13,26 @@ document.getElementById('textInput').addEventListener('input', () => {
         // Отображение элемента h2, если поле ввода не пустое
         h2.style.display = 'block'
         // Текст элемента h2 с информацией о вводимом тексте
-        h2.innerText = `Вcего символов: ${input.value.length}`;
+        h2.innerText = `Вcего символов: ${input.value.length}\n\nГласных букв: ${count}`;
+        // Изменение цвета текста h2 в зависимости от количества гласных
+        if (count > 4) {
+            h2.style.color = 'red';
+        } else {
+            h2.style.color = 'blue';
+        }
     }
 })
+
+// Фуекция для подсчёта количества гласных
+function getVowels(input) {
+    input = input.toLowerCase();
+    let count = 0;
+    // Создание массива из гласных
+    let vowels = ['а', 'у', 'о', 'и', 'э', 'ы', 'я', 'ю', 'е', 'ё'];
+    for (let i = 0; i < input.length; i++) {
+        if (vowels.includes(input[i])) {
+            count+=1;
+        }
+    }
+    return count;
+}
