3ebe7978c1a584e710664d130943ebdec538fd5f
areyshka <elena.draven@mail.ru>
2024-05-24 18:03:55 +0300

    feat: add the second task

--- Task-9/main.js
+++ Task-9/main.js
@@ -14,3 +14,18 @@
 //     }
 //     alert(reverseStr);
 // }
+
+// 2. Задача: Подсчет гласных 
+// Запросите у пользователя строку и выведите количество гласных букв в этой строке.
+
+// function countVowels() {
+//     let str = document.getElementById('inputString').value.toLowerCase();
+//     let vowels = ['а', 'у', 'о', 'и', 'э', 'ы', 'я', 'ю', 'е', 'ё', 'a', 'e', 'i', 'o', 'u'];
+//     let count = 0;
+//     for (let i = 0; i < str.length; i++) {
+//         if(vowels.includes(str[i])) {
+//             count++;
+//         }
+//     }
+//     alert(count);
+// }
