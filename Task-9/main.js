1c4359f44c2d57636b9af7c5d2a9f552b0216987
areyshka <elena.draven@mail.ru>
2024-05-24 17:52:41 +0300

    feat: add the first task

---
+++ Task-9/main.js
@@ -0,0 +1,16 @@
+// 1. Задача: Разворот строки asd dsa
+// Запросите у пользователя строку и выведите ее в обратном порядке.
+
+// function reverseString() {
+//     let str = document.getElementById('inputString').value;
+//     alert(str.split('').reverse().join(''));
+// }
+
+// function reverseString() {
+//     let str = document.getElementById('inputString').value;
+//     let reverseStr = '';
+//     for (let i = str.length - 1; i >= 0; i--) {
+//         reverseStr += str[i];
+//     }
+//     alert(reverseStr);
+// }
