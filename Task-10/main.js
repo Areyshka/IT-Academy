82e40dde6dae74065b4d2473ded532098b2d0e3c
areyshka <elena.draven@mail.ru>
2024-05-29 18:06:51 +0300

    feat: add the third task

--- Task-10/main.js
+++ Task-10/main.js
@@ -15,3 +15,20 @@
 
 // isOdd(10);
 
+// 3. Напишите программу, которая выводит числа от 1 до 100, заменяя числа, которые делятся на 3 на "Fizz", числа, которые делятся на 5 на "Buzz", а числа, которые делятся на оба на "FizzBuzz".
+
+// function numbers() {
+//     for (let i = 1; i <= 100; i++) {
+//         if (i % 3 === 0 && i % 5 === 0) {
+//             console.log("FizzBuzz");
+//         } else if (i % 3 === 0) {
+//             console.log("Fizz");
+//         } else if (i % 5 === 0) {
+//             console.log("Buzz");
+//         }
+//         console.log(i);
+//     } 
+// }
+
+// numbers();
+
