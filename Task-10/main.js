016927d871a53369086e5cf58bb8d0bda440cba3
areyshka <elena.draven@mail.ru>
2024-05-29 17:46:04 +0300

    feat: add the first task

---
+++ Task-10/main.js
@@ -0,0 +1,6 @@
+// 1. Запросить у пользователя два числа и вывести их сумму, разность, произведение и частное.
+
+// let firstNum = parseInt(prompt("Введите первое число."));
+// let secondNum = parseInt(prompt("Введите второе число."));
+// alert(`Сумма: ${firstNum + secondNum}\nРазность: ${firstNum - secondNum}\nПроизведение: ${firstNum * secondNum}\nЧастное: ${firstNum / secondNum}`);
+
