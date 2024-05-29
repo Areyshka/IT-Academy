a7c6a83fd6fbca6af00b60deb3f9bbea8c416801
areyshka <elena.draven@mail.ru>
2024-05-29 17:51:01 +0300

    feat: add the second task

--- Task-10/main.js
+++ Task-10/main.js
@@ -4,3 +4,14 @@
 // let secondNum = parseInt(prompt("Введите второе число."));
 // alert(`Сумма: ${firstNum + secondNum}\nРазность: ${firstNum - secondNum}\nПроизведение: ${firstNum * secondNum}\nЧастное: ${firstNum / secondNum}`);
 
+// 2. Напишите программу, которая проверяет, является ли введенное пользователем число четным или нечетным.
+
+// function isOdd(num) {
+//     if (num % 2 !== 0) {
+//         alert("Число нечётное.");
+//     }
+//         alert("Число чётное.");
+// }
+
+// isOdd(10);
+
