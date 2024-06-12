f8a2f30275c54bbf8ef41f95f393f5ff933e404d
areyshka <elena.draven@mail.ru>
2024-06-12 12:42:45 +0300

    feat: add generate password task

--- different tasks/main.js
+++ different tasks/main.js
@@ -26,3 +26,26 @@
 
 // let result = arr.reduce((word, elem) => word.length > elem.length ? word : elem);
 // text.textContent = result;
+
+// 3 Задание: Генератор случайных паролей
+// const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+// Создайте функцию, которая принимает на вход длину пароля.
+// Внутри функции сгенерируйте случайный пароль заданной длины из букв верхнего и нижнего регистра, а также из цифр.
+// Выведите сгенерированный пароль на экран.
+
+// let text = document.getElementById('text');
+// let num = parseFloat(prompt("Введите длину пароля (число):"));
+// const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+// function getPassword(num) {
+//     let result = "";
+//     for (let i = 0; i < num; i++) {
+//         let index = Math.floor(Math.random() * characters.length);
+//         result += characters[index];
+//     }
+//     return result;
+// }
+
+// let password = getPassword(num);
+// text.textContent = password;
+
