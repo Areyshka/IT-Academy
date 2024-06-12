8583f7b7e10dce09c5aa437a6979115a66c45023
areyshka <elena.draven@mail.ru>
2024-06-12 23:24:50 +0300

    feat: add code comments

--- Task-11/main.js
+++ Task-11/main.js
@@ -1,9 +1,13 @@
+// Событие, срабатывающее, когда HTML полностью загружен.
 document.addEventListener("DOMContentLoaded", function() {
+
+    // Получение всех необходимых элементов.
     let cells = document.querySelectorAll(".cell");
     let startAgain = document.getElementById("start");
     let message = document.getElementById("message");
     let player = document.getElementById("player");
     
+    // Массив выигрышных комбинаций.
     const winCombinations = [
         [0, 1, 2],
         [3, 4, 5],
@@ -15,24 +19,40 @@ document.addEventListener("DOMContentLoaded", function() {
         [2, 4, 6]
     ];
     
+    // Изначально пустое игровое поле.
     let playingField = ["", "", "", "", "", "", "", "", ""];
+
+    // Текущий игрок.
     let currentPlayer = "X";
+
+    // Состояние игры.
     let game = false;
     
+    // Функция начала игры.
     function startGame() {
         game = true;
+
+        // Обработчик события по клику для каждой ячейки.
         cells.forEach(cell => cell.addEventListener("click", () => {
             message.textContent = "";
+
+            // Получение индекса ячейки.
             let index = parseFloat(cell.getAttribute("index"));
+
+            // Проверка на заполненность ячейки.
             if(playingField[index] !== "" && game) {
                 message.textContent = "This cell is already occupied";
                 return;
         }
-    
+        
+        // Вызов функции обновления ячейки.
         updateCell(cell, index);
+
+        // Вызов функции проверки победы.
         isWin();
         }));
     
+        // Добавление обработчика событий для начала игры заново, приведение всех позиций в начальное состояние.
         startAgain.addEventListener("click", () => {
             playingField = ["", "", "", "", "", "", "", "", ""];
             currentPlayer = "X";
@@ -46,6 +66,7 @@ document.addEventListener("DOMContentLoaded", function() {
         });  
     };
     
+    // Функция обновления ячейки.
     function updateCell(cell, index) {
         if (playingField[index] === "" && game) {
             playingField[index] = currentPlayer;
@@ -54,13 +75,18 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     };
     
+    // Функция для смены игрока.
     function switchPlayer() {
         currentPlayer = currentPlayer === "X" ? "O" : "X";
         player.textContent = currentPlayer;
     };
     
+
+    // Функция проверки победы.
     function isWin() {
         let win = false;
+
+        // Проверка выигрышных комбинаций.
         for (let i = 0; i < winCombinations.length; i++) {
             let comb = winCombinations[i];
             let one = playingField[comb[0]];
@@ -77,7 +103,8 @@ document.addEventListener("DOMContentLoaded", function() {
                 break;
             }
         }
-    
+        
+        // Проверка, есть ли победитель или ничья.
         if (win) {
             player.textContent = `${currentPlayer}`;
             game = false;
@@ -89,6 +116,7 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     };
     
+    // Вызов функции начала игры.
     startGame();
     
     });
